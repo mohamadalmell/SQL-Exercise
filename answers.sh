10b096140a258139be264ec668551ce1205f73ae
mohamadalmell <mohamadalmell@gmail.com>
2021-12-13 12:42:56 +0200

    Create Table

--- answers.sh
+++ answers.sh
@@ -25,3 +25,28 @@ where Name='Basma'
 UPDATE students
 set points=points-100
 where name='Alex'
+
+#Creating tables
+CREATE TABLE graduates (
+ID INTEGER PRIMARY KEY AUTOINCREMENT,
+Name TEXT NOT NULL UNIQUE,
+Age INTEGER,
+Gender TEXT,
+Points INTEGER,
+Graduation date
+);
+
+#1)Copy Layal's data from students to graduates
+INSERT INTO graduates(Name, Age, Gender, Points)
+SELECT Name, Age, Gender, Points FROM students
+where ID=4
+
+#2)Add the graduation date previously mentioned to Layal's record in graduates
+UPDATE graduates
+set Graduation='08-09-2018'
+WHERE ID=1
+
+#3)Remove Layal's record from students
+DELETE FROM students
+WHERE id=4
+
